c469d3cd0aca865654e352cae3875b9fbcee0bba
nicocorbi <nicolascorbi2002@gmail.com>
2025-06-10 17:26:29 +0000

    puesta de imagenes de hambrguesas en el apartado menu

--- data/menu.js
+++ data/menu.js
@@ -1,5 +1,22 @@
 module.exports = [
-    { name: 'Paella', price: 12.99 },
-    { name: 'Gazpacho', price: 8.99 },
-    { name: 'Tortilla Española', price: 6.99 },
-  ];
+  {
+    name: 'Hamburguesa Clásica',
+    price: 8.99,
+    image: '/hamburguesa_clasica.jpg'
+  },
+  {
+    name: 'Hamburguesa BBQ',
+    price: 9.99,
+    image: '/hamburguesa_bbq.jpg'
+  },
+  {
+    name: 'Hamburguesa Vegana',
+    price: 10.99,
+    image: '/hamburguesa_vegana.jpg'
+  },
+  {
+    name: 'Hamburguesa con Queso',
+    price: 9.49,
+    image: '/hamburguesa_queso.jpg'
+  }
+];
