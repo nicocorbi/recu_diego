1774542b8c643db2e8efd3e155a6f73a79e605c5
nicocorbi <nicolascorbi2002@gmail.com>
2025-06-11 17:30:16 +0000

    mejora en el fondo del menu para que se parezca al resto y he añadido una pagina de descripcion de la hamburguesa al darle a pedir

--- routes/menu.js
+++ routes/menu.js
@@ -3,7 +3,16 @@ const router = express.Router();
 const menuData = require('../data/menu');
 
 router.get('/', (req, res) => {
-  res.render('vistas.njk', { menu: menuData }); // Cambia a .njk
+  res.render('vistas.njk', { menu: menuData });
+});
+
+// Ruta para detalle de hamburguesa
+router.get('/menu/:id', (req, res) => {
+  const burger = menuData[req.params.id];
+  if (!burger) {
+    return res.status(404).send('Hamburguesa no encontrada');
+  }
+  res.render('detalle.njk', { burger });
 });
 
 module.exports = router;
